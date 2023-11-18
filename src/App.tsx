70464b123d1f67de148d6492757d3b31d0a2148f
Purice Victor <pvictor0110@gmail.com>
2023-11-18 22:17:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Notes App</div>;
+interface Note {
+  id: number;
+  title: string;
+  content: string;
 }
+
+const App: React.FC = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const addNote = () => {
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: "New Note",
+      content: "Write your content here",
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  return (
+    <div className="app">
+      <button onClick={addNote}>Add Note</button>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <h3>{note.title}</h3>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
