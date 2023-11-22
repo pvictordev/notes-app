f8dcb07ab96e23165d233232b4bea4f382cb6d35
Purice Victor <pvictor0110@gmail.com>
2023-11-22 20:44:51 +0200

    removed commented code

--- src/App.tsx
+++ src/App.tsx
@@ -16,23 +16,13 @@ interface Note {
 
 export default function App(): JSX.Element {
   //removed because i will no longer save data on localStorage
-  // const [notes, setNotes] = useState<Note[]>(() =>
-  //   JSON.parse(localStorage.getItem("notes") || "[]")
-  // );
   const [notes, setNotes] = useState<Note[]>([]);
 
   //is still working but replaced with the optional chaining operator bellow
-  // const [curNoteId, setCurNoteId] = useState<string>(
-  //   (notes[0] && notes[0].id) || ""
-  // );
   // const [curNoteId, setCurNoteId] = useState<string | undefined>(notes[0]?.id);
   const [curNoteId, setCurNoteId] = useState<string>("");
 
   //removed because i will no longer save data on localStorage
-  // useEffect(() => {
-  //   localStorage.setItem("notes", JSON.stringify(notes));
-  // }, [notes]);
-
   useEffect(() => {
     //created websocket connection to firebase
     const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
@@ -52,82 +42,18 @@ export default function App(): JSX.Element {
   }, [notes]);
 
   //refactored for firebase
-  // const updateNote = (text: string) => {
-  //   //doest not put updated note at the top of the list
-  //   // setNotes((oldNotes) => {
-  //   //   const updatedArr = oldNotes.map((oldNote) =>
-  //   //     oldNote.id === curNoteId ? { ...oldNote, body: text } : oldNote
-  //   //   );
-  //   //   return updatedArr;
-  //   // });
-
-  //   //puts updated note at the top of the list
-  //   // setNotes((oldNotes) => {
-  //   //   const newArray = [];
-  //   //   for (let i = 0; i < oldNotes.length; i++) {
-  //   //     const oldNote = oldNotes[i];
-  //   //     if (oldNote.id === curNoteId) {
-  //   //       newArray.unshift({ ...oldNote, body: text });
-  //   //     } else {
-  //   //       newArray.push(oldNote);
-  //   //     }
-  //   //   }
-  //   //   return newArray;
-  //   // });
-
-  //   // puts updated note at the top of the list (modified)
-  //   setNotes((oldNotes) => {
-  //     const updatedNoteIndex = oldNotes.findIndex(
-  //       (note) => note.id === curNoteId
-  //     );
-  //     if (updatedNoteIndex === -1) {
-  //       return oldNotes;
-  //     }
-
-  //     const updatedNote = { ...oldNotes[updatedNoteIndex], body: text };
-  //     const updatedNotes = [
-  //       updatedNote,
-  //       ...oldNotes.slice(0, updatedNoteIndex),
-  //       ...oldNotes.slice(updatedNoteIndex + 1),
-  //     ];
-
-  //     return updatedNotes;
-  //   });
-  // };
   const updateNote = async (text: string) => {
     const docRef = doc(db, "notes", curNoteId);
     await setDoc(docRef, { body: text, updateAt: Date.now() }, { merge: true });
   };
 
   //refactored for firebase
-  // const deleteNote = (
-  //   event: React.MouseEvent<HTMLButtonElement>,
-  //   noteId: string
-  // ) => {
-  //   event.stopPropagation();
-  //   setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
-  // };
   const deleteNote = async (noteId: string) => {
     const docRef = doc(db, "notes", noteId);
     await deleteDoc(docRef);
   };
 
   //refactored for firebase
-  // const createNewNote = () => {
-  //   const newNote: Note = {
-  //     id: nanoid(),
-  //     body: "# Type your markdown note title here",
-  //   };
-  //   setNotes((prevNotes) => {
-  //     if (!Array.isArray(prevNotes)) {
-  //       console.error("Notes is not an array:", prevNotes);
-  //       return [newNote];
-  //     }
-  //     return [newNote, ...prevNotes];
-  //   });
-
-  //   setCurNoteId(newNote.id);
-  // };
   async function createNewNote() {
     const newNote: Note = {
       body: "# Type your markdown note title here",
@@ -140,12 +66,6 @@ export default function App(): JSX.Element {
   }
 
   //replaced for performance with the line bellow
-  // const findCurrentNote = (): Note => {
-  //   return (
-  //     notes.find((note) => note.id === curNoteId) ||
-  //     notes[0] || { id: "", body: "" }
-  //   );
-  // };
   const currentNote = notes.find((note) => note.id === curNoteId) || notes[0];
 
   return (
