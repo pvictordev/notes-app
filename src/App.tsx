c52fa0bfd17f8a2fc37dc22ba5b26812adfce9a5
Purice Victor <pvictor0110@gmail.com>
2023-11-22 22:22:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { notesCollection, db } from "./firebase";
 
 // React MDE style
 import "react-mde/lib/styles/css/react-mde-all.css";
-
 interface Note {
   id: string;
   body: string;
@@ -23,6 +22,10 @@ export default function App(): JSX.Element {
   //is still working but replaced with the optional chaining operator bellow
   // const [curNoteId, setCurNoteId] = useState<string | undefined>(notes[0]?.id);
   const [curNoteId, setCurNoteId] = useState<string>("");
+
+  const currentNote = notes.find((note) => note.id === curNoteId) || notes[0];
+
+  const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt);
 
   //removed because i will no longer save data on localStorage
   useEffect(() => {
@@ -44,6 +47,17 @@ export default function App(): JSX.Element {
   }, [notes]);
 
   //refactored for firebase
+  async function createNewNote() {
+    const newNote: Note = {
+      body: "# Type your markdown note title here",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    const newNoteRef = await addDoc(notesCollection, newNote);
+    setCurNoteId(newNoteRef.id);
+  }
+
+  //refactored for firebase
   const updateNote = async (text: string) => {
     const docRef = doc(db, "notes", curNoteId);
     await setDoc(docRef, { body: text, updateAt: Date.now() }, { merge: true });
@@ -55,21 +69,6 @@ export default function App(): JSX.Element {
     await deleteDoc(docRef);
   };
 
-  //refactored for firebase
-  async function createNewNote() {
-    const newNote: Note = {
-      body: "# Type your markdown note title here",
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    };
-    const newNoteRef = await addDoc(notesCollection, newNote);
-
-    setCurNoteId(newNoteRef.id);
-  }
-
-  //replaced for performance with the line bellow
-  const currentNote = notes.find((note) => note.id === curNoteId) || notes[0];
-
   return (
     <main>
       {notes.length > 0 ? (
@@ -78,7 +77,7 @@ export default function App(): JSX.Element {
             newNote={createNewNote}
             currentNote={currentNote}
             setCurNoteId={setCurNoteId}
-            notes={notes}
+            notes={sortedNotes}
             deleteNote={deleteNote}
           />
 
